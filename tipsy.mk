d3953e6375f951561ccfe2cfddff0cd86128b53b
Vatsal <koradiavatsal@gmail.com>
2017-01-23 15:46:30 +0530

    Tipsy
    Change-Id: If99027337b13d88a4b6cff49c5ef792b551cb792

--- tipsy.mk
+++ tipsy.mk
@@ -18,14 +18,13 @@
 $(call inherit-product, device/xiaomi/hydrogen/full_hydrogen.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
-$(call inherit-product, vendor/tesla/config/caf_fw.mk)
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := hydrogen
-PRODUCT_NAME := tesla_hydrogen
+PRODUCT_NAME := tipsy_hydrogen
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Mi Max
 TARGET_VENDOR := Xiaomi
